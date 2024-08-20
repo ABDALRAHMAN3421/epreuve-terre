616ccaa9e28fd96108dd4b9a1c959f228027a81e
T <166978705+ABDALRAHMAN3421@users.noreply.github.com>
2024-08-20 17:00:09 +0400

    Delete terre[10],[terre10].py

--- terre[10],[terre10].py
+++
@@ -1,8 +0,0 @@
-def est_premier(nombre):
-    return nombre > 1 and all(nombre % i != 0 for i in range(2, int(nombre**0.5) + 1))
-
-nombre = int(input("Entrez un nombre entier positif: "))
-if est_premier(nombre):
-    print(nombre, "est un nombre premier.")
-else:
-    print(nombre, "n'est pas un nombre premier.")
