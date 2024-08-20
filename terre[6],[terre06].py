ef267e3aed5d43d401437e9bac45b9299078d178
T <166978705+ABDALRAHMAN3421@users.noreply.github.com>
2024-08-20 17:01:40 +0400

    Delete terre[6],[terre06].py

--- terre[6],[terre06].py
+++
@@ -1,9 +0,0 @@
-def reverseString(phrase):
-    return phrase[::-1]
-
-user_input = input("Please enter a phrase: ")
-
-if user_input.isdigit():
-    print("Please enter a phrase containing alphabetic characters.")
-else:
-    print(reverseString(user_input))
